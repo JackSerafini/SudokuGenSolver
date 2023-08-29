d4ab7477b03b2fe83f7c046997e306b2c92ddd1b
JackSerafini <lagiacromo2803@gmail.com>
2023-08-29 18:32:45 +0200

    Small changes but it keeps goint into a loop :(

--- SudokuGen.c
+++ SudokuGen.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
-#include <stdbool.h>
 
 // My idea is to create a 9x9 grid and fill it with randon numbers, checking per each cell if the put value is acceptable.
 // After that, deleting K values from the grid randomly, we should have the final product.
@@ -18,72 +17,55 @@
 // Definition of the number of rows and columns of the matrix
 #define ROWS 9 
 #define COLUMNS 9
+
 int mat[ROWS][COLUMNS];
 int N = 9;
 
 
-bool CheckTOT();
-void FillSudoku();
-
-
-int main(void){
-    printf("Hello World\n");
-
-    return 0;
-}
-
 // Function to check if the number is not used in the row
-bool NotUsedInRow(int number, int row){
-    //bool check = true;
+int NotUsedInRow(int number, int row){
     for(int j=1; j<=COLUMNS; j++){
         if(number == mat[row][j])
-            //check = false;
-            return false;
+            return 0;
     }
-    //return check;
-    return true;
+    return 1;
 }
 
 // Function to check if the number is not used in the column
-bool NotUsedInColumn(int number, int column){
-    //bool check = true;
+int NotUsedInColumn(int number, int column){
     for(int i=1; i<=ROWS; i++){
         if(number == mat[i][column])
-            //check = false;
-            return false;
+            return 0;
     }
-    //return check;
-    return true;
+    return 1;
 }
 
-// Function to check if the number is not used in the 3x3 grid
-// FOr now I've just added the diagonal grids, even though it sounds strange to me that I'd have to add all nine of them manually
-bool NotUsedIn3x3Grid(int number, int row, int column){
-    //bool check = true;
+// Function to check if the number is not used in the 3x3 grids
+int NotUsedIn3x3Grid(int number, int row, int column){
+    // Top-Left Grid
     if(row >= 1 && row <= 3 && column >= 1 && column <= 3){
         for(int i=1; i<=3; i++){
             for(int j=1; j<=3; j++){
                 if(number == mat[i][j])
-                    //check = false;
-                    return false;
+                    return 0;
             }
         }
     }
+    // Top-Centre Grid
     if(row >= 1 && row <= 3 && column >= 4 && column <= 6){
         for(int i=1; i<=3; i++){
             for(int j=4; j<=6; j++){
                 if(number == mat[i][j])
-                    //check = false;
-                    return false;
+                    return 0;
             }
         }
     }
+    // Top-Right Grid
     if(row >= 1 && row <= 3 && column >= 7 && column <= 9){
         for(int i=1; i<=3; i++){
             for(int j=7; j<=9; j++){
                 if(number == mat[i][j])
-                    //check = false;
-                    return false;
+                    return 0;
             }
         }
     }
@@ -92,8 +74,7 @@ bool NotUsedIn3x3Grid(int number, int row, int column){
         for(int i=4; i<=6; i++){
             for(int j=1; j<=3; j++){
                 if(number == mat[i][j])
-                    //check = false;
-                    return false;
+                    return 0;
             }
         }
     }
@@ -102,8 +83,7 @@ bool NotUsedIn3x3Grid(int number, int row, int column){
         for(int i=4; i<=6; i++){
             for(int j=4; j<=6; j++){
                 if(number == mat[i][j])
-                    //check = false;
-                    return false;
+                    return 0;
             }
         }
     }
@@ -112,8 +92,7 @@ bool NotUsedIn3x3Grid(int number, int row, int column){
         for(int i=4; i<=6; i++){
             for(int j=7; j<=9; j++){
                 if(number == mat[i][j])
-                    //check = false;
-                    return false;
+                    return 0;
             }
         }
     }
@@ -122,8 +101,7 @@ bool NotUsedIn3x3Grid(int number, int row, int column){
         for(int i=7; i<=9; i++){
             for(int j=1; j<=3; j++){
                 if(number == mat[i][j])
-                    //check = false;
-                    return false;
+                    return 0;
             }
         }
     }
@@ -132,8 +110,7 @@ bool NotUsedIn3x3Grid(int number, int row, int column){
         for(int i=7; i<=9; i++){
             for(int j=4; j<=6; j++){
                 if(number == mat[i][j])
-                    //check = false;
-                    return false;
+                    return 0;
             }
         }
     }
@@ -142,36 +119,49 @@ bool NotUsedIn3x3Grid(int number, int row, int column){
         for(int i=7; i<=9; i++){
             for(int j=7; j<=9; j++){
                 if(number == mat[i][j])
-                    //check = false;
-                    return false;
+                    return 0;
             }
         }
     }
-    //return check;
-    return true;
+    return 1;
 }
 
 // Sum of all the checks
-bool CheckTOT(int number, int row, int column){
-    if(NotUsedInRow(number, row) == true && NotUsedInColumn(number, column) == true && NotUsedIn3x3Grid(number, row, column) == true)
-        return true;
-    else return false;
+int CheckTOT(int number, int row, int column){
+    if(NotUsedInRow(number, row) == 1 && NotUsedInColumn(number, column) == 1 && NotUsedIn3x3Grid(number, row, column) == 1)
+        return 1;
+    else return 0;
 }
 
 // Function to fill the grid
-void FillSudoku(int mat){
+void FillSudoku(){
     srand(time(NULL));
     int number;
     for(int i=0; i<ROWS; i++){
         for(int j=0; j<COLUMNS; j++){
-            int flag = false;
-            while(flag == false){
+            int flag = 0;
+            while(flag == 0){
                 int randNumber = (rand() % 9) + 1;
-                if(CheckTOT(randNumber, i, j) == true) 
-                    mat[&i][&j] = randNumber;
-                    flag = true;
+                if(CheckTOT(randNumber, i, j) == 1) 
+                    flag = 1;
+            mat[i][j] = randNumber;
             }
         }
     }
     return;
+}
+
+
+int main(void){
+    printf("Hello World\n");
+    FillSudoku();
+
+    for(int i=0;i<ROWS;i++){
+        for(int j=0;j<COLUMNS;j++){
+            printf("%d ",mat[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
